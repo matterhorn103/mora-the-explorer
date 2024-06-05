b519c810bad458d97b4da1a2fac6260ffdb77697
Matthew J. Milner <matterhorn103@proton.me>
2024-06-05 18:19:10 +0200

    Add the spinner source files

---
+++ mora_the_explorer/ui/status.py
@@ -0,0 +1,39 @@
+from PySide6.QtWidgets import QWidget, QHBoxLayout, QLabel
+
+from .spinner import WaitingSpinner
+
+
+class StatusBar(QWidget):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.layout = QHBoxLayout()
+        self.setLayout(self.layout)
+        self.label = QLabel("checking...")
+        self.spinner = WaitingSpinner(
+            self,
+            roundness=100.0,
+            opacity=3.141592653589793,
+            fade=80.0,
+            radius=5,
+            lines=20,
+            line_length=10,
+            line_width=2,
+            speed=1.5707963267948966,
+            color=(184, 140, 206)
+        )
+        self.layout.addWidget(self.label)
+        self.layout.addWidget(self.spinner)
+
+    def setText(self, text):
+        self.label.setText(text)
+
+    def hide(self):
+        self.label.hide()
+        self.spinner.hide()
+
+    def show(self):
+        self.label.show()
+        self.spinner.show()
+
