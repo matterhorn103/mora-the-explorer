2f1785c075cdcebc22261f39009d351ad354e37c
Matt Milner <matterhorn103@proton.me>
2024-07-01 21:15:44 +0200

    Add clickable link to send bug report with some pre-filled information (#10)

--- mora_the_explorer/desktop/ui/layout.py
+++ mora_the_explorer/desktop/ui/layout.py
@@ -25,9 +25,16 @@ class Layout(QVBoxLayout):
         # Title and version info header
         with open(resource_directory / "version.txt", encoding="utf-8") as f:
             version_info = "".join(f.readlines()[:5])
-        version_box = QLabel(version_info)
-        version_box.setAlignment(Qt.AlignHCenter)
-        self.addWidget(version_box)
+        # Turn into html
+        version_info = f'<p style="line-height: 1.1;">{version_info.replace("\n", "<br>")}</p>'
+        self.version_box = QLabel(version_info)
+        self.version_box.setAlignment(Qt.AlignHCenter)
+        self.addWidget(self.version_box)
+
+        # Link to send bug report
+        self.bug_report_link = QLabel('<a href="#">Report a bug</a>')
+        self.bug_report_link.setAlignment(Qt.AlignHCenter)
+        #self.addWidget(self.bug_report_link)
 
         # All the user-configurable options
         self.opts = OptionsLayout(config)
