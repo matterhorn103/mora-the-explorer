5ddbf56d4954652804b48ec220e41ddf807351e3
Matthew Milner <matterhorn103@proton.me>
2026-06-08 23:48:06 +0200

    Use default dark mode and rely on Qt's automatic dark mode detection

--- src/mora_the_explorer/desktop/app.py
+++ src/mora_the_explorer/desktop/app.py
@@ -1,12 +1,10 @@
+from PySide6.QtGui import QIcon
 from typing import cast
 import logging
 from pathlib import Path
 import platform
 
-import darkdetect
-
 from PySide6.QtCore import Qt, QFile, QIODevice
-from PySide6.QtGui import QPalette, QColor, QIcon, QColorConstants
 from PySide6.QtWidgets import QApplication
 
 from .. import LOG_FILE, get_rsrc_dir
@@ -37,6 +35,8 @@ class App:
         """Create a Mora the Explorer desktop application with a GUI, but don't run it yet."""
 
         self.app = QApplication()
+        rsrc_dir = get_rsrc_dir()
+        self.app.setWindowIcon(QIcon(str(rsrc_dir / "explorer.ico")))
 
         logging.basicConfig(
             filename=LOG_FILE,
@@ -45,8 +45,6 @@ class App:
             encoding="utf-8",
             level=logging.INFO,
         )
-
-        rsrc_dir = get_rsrc_dir()
 
         logging.info("Loading program settings…")
         if app_config_file is None:
@@ -57,35 +55,7 @@ class App:
         # Create singleton instance of `Controller` to handle the various components
         self.controller = Controller(config)
 
-        if darkdetect.isDark() is True and platform.system() == "Windows":
-            self.set_dark_mode()
-
-        self.app.setWindowIcon(QIcon(str(rsrc_dir / "explorer.ico")))
-
         logging.info("Initialization complete")
-
-    def set_dark_mode(self):
-        """Manually set a dark mode (intended for use on Windows).
-
-        Make dark mode less black than Windows default dark mode because it looks bad.
-        """
-
-        dark_palette = QPalette()
-        dark_palette.setColor(QPalette.ColorRole.Window, QColor(53, 53, 53))
-        dark_palette.setColor(QPalette.ColorRole.WindowText, QColorConstants.White)
-        dark_palette.setColor(QPalette.ColorRole.Base, QColor(25, 25, 25))
-        dark_palette.setColor(QPalette.ColorRole.AlternateBase, QColor(53, 53, 53))
-        dark_palette.setColor(QPalette.ColorRole.ToolTipBase, QColorConstants.Black)
-        dark_palette.setColor(QPalette.ColorRole.ToolTipText, QColorConstants.White)
-        dark_palette.setColor(QPalette.ColorRole.Text, QColorConstants.White)
-        dark_palette.setColor(QPalette.ColorRole.Button, QColor(53, 53, 53))
-        dark_palette.setColor(QPalette.ColorRole.ButtonText, QColorConstants.White)
-        dark_palette.setColor(QPalette.ColorRole.BrightText, QColorConstants.Red)
-        dark_palette.setColor(QPalette.ColorRole.Link, QColor(42, 130, 218))
-        dark_palette.setColor(QPalette.ColorRole.Highlight, QColor(42, 130, 218))
-        dark_palette.setColor(QPalette.ColorRole.HighlightedText, QColorConstants.Black)
-        self.app.setStyle("Fusion")
-        self.app.setPalette(dark_palette)
 
     def run(self) -> int:
         """Run the Mora the Explorer desktop application.
